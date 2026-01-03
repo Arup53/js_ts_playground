99d7af86de051a8ec6331da2d1a972ca28d1feb0
Arup53 <arupkumarhowlader12@gmail.com>
2026-01-03 10:40:49 +0600

    add types in app.ts

--- rate_limiter/src/app.ts
+++ rate_limiter/src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import { Request, Response } from "express";
 import { rateLimiter } from "./rate_limiter";
 
 const app = express();
 const PORT = process.env.PORT ?? 8080;
 
-app.get("/", rateLimiter, async (req, res) => {
+app.get("/", rateLimiter, async (req: Request, res: Response) => {
   return res.json({ message: "Hello" });
 });
 
