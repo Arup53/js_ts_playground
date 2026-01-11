ce69a60f74522228c8866ab0d16d0ad08415e5ba
Arup53 <arupkumarhowlader12@gmail.com>
2026-01-11 10:50:22 +0600

    feat:implement one to one chat logic

--- websocket/src/index.ts
+++ websocket/src/index.ts
@@ -2,17 +2,33 @@ import Websocket from "ws";
 
 const wss = new Websocket.Server({ port: 8080 });
 
+const users = new Map<string, Websocket>();
+
 wss.on("connection", (ws) => {
   ws.send("Connected to websocket server");
 
-  ws.on("message", (message) => {
-    const text = message.toString();
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(`Broadcast: ${message}`);
-      }
-    });
-    console.log("Recived", text);
+  ws.on("message", (data) => {
+    const text = data.toString();
+    // wss.clients.forEach((client) => {
+    //   if (client.readyState === WebSocket.OPEN) {
+    //     client.send(`Broadcast: ${message}`);
+    //   }
+    // });
+
+    const msg = JSON.parse(text);
+
+    // 1to1 chat implemention
+    if (msg.type === "join_user") {
+      users.set(msg.user_id, ws);
+    }
+
+    if (msg.type === "private_message") {
+      const user = users.get(msg.to);
+      console.log("message body is ", msg.body);
+      user?.send(JSON.stringify(msg.body));
+    }
+
+    console.log("Recived", msg);
     ws.send(`Server received:,${text}`);
   });
 
