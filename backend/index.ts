c32992c22707c0f4ef677a52313374ba71473e99
Arup53 <arupkumarhowlader12@gmail.com>
2026-02-03 22:05:08 +0600

    chores: delte unncessary comments

--- backend/index.ts
+++ backend/index.ts
@@ -4,16 +4,12 @@ import { anonEventsRegisters } from "./routes/events_register_anon";
 const app = express();
 const server_port = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   console.log("Received request to ", req);
-//   res.json({ message: "Hello from server" });
-// });
-app.get('/',async(req,res)=>{
-  res.send("Hello from server")
-})
-app.use("/backend/routes/events_register_anon",anonEventsRegisters)
+app.get("/", async (req, res) => {
+  res.send("Hello from server");
+});
+app.use("/backend/routes/events_register_anon", anonEventsRegisters);
 
 try {
   const server = app.listen(server_port, () => {
