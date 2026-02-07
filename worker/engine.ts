bc6ea321a09abd7545474a95dc534f82fa29b3e3
Arup53 <arupkumarhowlader12@gmail.com>
2026-02-07 10:13:32 +0600

    feat: implement hashmap to store all campaigns of a tenant

--- worker/engine.ts
+++ worker/engine.ts
@@ -11,9 +11,73 @@ interface Campaigns {
     eventName: string;
   };
   status: Status.Active | Status.Completed;
-  TIMESTAMP: string;
+  notification_services: [];
+  template: string; // s3 url
+  start_at: string;
+  end_at: string;
 }
 
-async function process() {
-  const campaigns = [];
+const campaigns = [
+  {
+    campaign_id: 1,
+    trigger: {
+      event: "sign_up",
+    },
+  },
+  {
+    campaign_id: 2,
+    trigger: {
+      event: "logged_in_30days",
+    },
+  },
+  {
+    campaign_id: 3,
+    trigger: {
+      event: "cart_abadonment",
+    },
+  },
+  {
+    campaign_id: 4,
+    trigger: {
+      event: "purchase",
+    },
+  },
+];
+
+const event = {
+  tenant_id: "abc1",
+  person_id: 42,
+  anonymous_id: "jiglypuf",
+  event: "sign_up",
+};
+
+// async function main(event) {
+//   const campaigns: any = [];
+//   for (const campaign of campaigns) {
+//     process(campaign, event);
+//   }
+// }
+
+function matchCampaigns(event, campaigns) {
+  const campaigns_Map: any = {};
+
+  for (const campaign of campaigns) {
+    if (campaign.trigger.event !== Object.keys(campaigns_Map))
+      campaigns_Map[campaign.trigger.event] = campaign;
+  }
+  console.log(campaigns_Map);
 }
+
+// async function process(campaign, event) {
+//   if (campaign.status === Status.Completed) {
+//     return "campaign completed";
+//   }
+//   if (!event.person_id) {
+//     return "No anonymous event processing";
+//   }
+
+//   if (event.event === campaign.trigger.eventName) {
+//   }
+// }
+
+matchCampaigns(event, campaigns);
