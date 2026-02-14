6d83232aad45256325eb13e2006f4085db38a1bf
Arup53 <arupkumarhowlader12@gmail.com>
2026-02-14 21:18:01 +0600

    feat: implement addCampaign method in cacheService

---
+++ worker/src/engine/services/cacheService.ts
@@ -0,0 +1,53 @@
+import { createClient, type RedisClientType } from "redis";
+
+class CacheService {
+  private client: RedisClientType | null;
+  private isConnected: boolean;
+  constructor() {
+    this.client = null;
+    this.isConnected = false;
+  }
+
+  async connect() {
+    if (this.isConnected) return;
+
+    this.client = createClient({
+      url: "redis://localhost:6379",
+    });
+
+    this.client.on("error", (err) => {
+      console.log("Redis connection error", err);
+      this.isConnected = false;
+    });
+
+    await this.client.connect();
+    this.isConnected = true;
+    console.log("Publisher conencted to Redis");
+  }
+
+  async addCampaign(campaign) {
+    if (!this.isConnected) {
+      await this.connect();
+    }
+
+    if (campaign) {
+      return "Error, Invalid Arguments";
+    }
+
+    const key = `campaign:${campaign.id}`;
+
+    const setCampaign = await this.client?.hSet(key, {
+      id: campaign.id.toString(),
+      tenant_id: campaign.tenant_id,
+      campaign_name: campaign.campaign_name,
+      campaign_type: campaign.campaign_type,
+      trigger: campaign.trigger,
+      actions: campaign.actions,
+      duration: campaign.duration,
+      frequency: campaign.frequency,
+      entries_customers: campaign.entries_customers,
+      active: campaign.active,
+      created_at: campaign.created_at.toISOString(),
+    });
+  }
+}
