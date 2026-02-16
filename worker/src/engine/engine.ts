38b53033b81434f2121f943ab20484582731a268
Arup53 <arupkumarhowlader12@gmail.com>
2026-02-16 13:42:33 +0600

    feat(engine): implement engine class

--- worker/src/engine/engine.ts
+++ worker/src/engine/engine.ts
@@ -1,4 +1,5 @@
 import Publisher from "./publisher";
+import CampaignService from "./services/campaignService";
 import {
   CamapaignTypes,
   Channel,
@@ -59,48 +60,131 @@ const campaigns: Campaign[] = [
   },
 ];
 
-let matched_campagins: Campaign[] = [];
+// let matched_campagins: Campaign[] = [];
 
-function fethActiveCampaigns(tenant_id) {}
+// function processMatchedCampaigns(event, matched_campagins) {
+//   matched_campagins.forEach((campaign: Campaign) => {
+//     campaign.actions.action.forEach((action: Action) => {
+//       campaign.trigger.conditions.forEach((condition) => {
+//         if (
+//           condition.base_condition &&
+//           campaign.frequency === 1 &&
+//           !Object.hasOwn(campaign.entries_customers, event.user_id)
+//         ) {
+//           console.log(action);
+//           publisher.publish(
+//             action.channel,
+//             JSON.stringify({
+//               message: action.message,
+//               user_id: event.user_id,
+//               tenant_id: tenant_id,
+//             })
+//           );
+//           campaign.entries_customers[event.user_id] = "subscribed";
+//         } else if (condition.segment_filter) {
+//         } else if (condition.attrubute_filter) {
+//         }
+//       });
+//     });
+//   });
+// }
 
-function matchCampaigns(event, activeCampaigns) {
-  for (const campaign of activeCampaigns) {
-    if (event.event === campaign.trigger.event_name) {
-      //also check for tenant_id
-      console.log(event.event);
-      console.log(campaign.trigger.event_name);
+// matchCampaigns(event1, campaigns);
+// processMatchedCampaigns(event1, matched_campagins);
 
-      matched_campagins.push(campaign);
+export class Engine {
+  private publisher: Publisher | null;
+  private campaignService: CampaignService | null;
+
+  constructor(publisher: Publisher, campaignService: CampaignService) {
+    this.publisher = publisher;
+    this.campaignService = campaignService;
+  }
+
+  async fethActiveCampaigns(eventTenantId, status = true) {
+    const campaigns = await this.campaignService?.getCampaignsOfTenantByStatus(
+      eventTenantId,
+      status
+    )!;
+    if (campaigns?.length === 0) {
+      return "Error, Engine failed to fetch campaigns";
     }
+    return campaigns;
+  }
+
+  async matchedCampaigns(event, activeCampaigns) {
+    let matched_campagins: Campaign[] = [];
+    for (const campaign of activeCampaigns) {
+      if (event.event === campaign.trigger.event_name) {
+        //also check for tenant_id
+        console.log(event.event);
+        console.log(campaign.trigger.event_name);
+
+        matched_campagins.push(campaign);
+      }
+    }
+    return matched_campagins;
+  }
+
+  async processMatchedCampaigns(event, matched_campagins) {
+    let result = true;
+
+    try {
+      matched_campagins.forEach((campaign: Campaign) => {
+        campaign.actions.action.forEach((action: Action) => {
+          campaign.trigger.conditions.forEach((condition) => {
+            if (
+              condition.base_condition &&
+              campaign.frequency === 1 &&
+              !Object.hasOwn(campaign.entries_customers, event.user_id)
+            ) {
+              console.log(action);
+              publisher.publish(
+                action.channel,
+                JSON.stringify({
+                  message: action.message,
+                  user_id: event.user_id,
+                  tenant_id: tenant_id,
+                })
+              );
+              campaign.entries_customers[event.user_id] = "subscribed";
+            } else if (condition.segment_filter) {
+            } else if (condition.attrubute_filter) {
+            }
+          });
+        });
+      });
+      return true;
+    } catch (e) {
+      console.log("Error, in processMatchedCampaigns method in engine class");
+      result = false;
+    }
+  }
+
+  async process(event) {
+    if (this.publisher || this.campaignService) {
+      return "Error, object not instanited in engine";
+    }
+
+    const active_campaigns = await this.fethActiveCampaigns(event.tenant_id);
+    const matched_campagins = await this.matchedCampaigns(
+      event,
+      active_campaigns
+    );
+    const result = await this.processMatchedCampaigns(event, matched_campagins);
+    console.log("worker has completed processing event", event.event);
   }
 }
 
-function processMatchedCampaigns(event, matched_campagins) {
-  matched_campagins.forEach((campaign: Campaign) => {
-    campaign.actions.action.forEach((action: Action) => {
-      campaign.trigger.conditions.forEach((condition) => {
-        if (
-          condition.base_condition &&
-          campaign.frequency === 1 &&
-          !Object.hasOwn(campaign.entries_customers, event.user_id)
-        ) {
-          console.log(action);
-          publisher.publish(
-            action.channel,
-            JSON.stringify({
-              message: action.message,
-              user_id: event.user_id,
-              tenant_id: tenant_id,
-            })
-          );
-          campaign.entries_customers[event.user_id] = "subscribed";
-        } else if (condition.segment_filter) {
-        } else if (condition.attrubute_filter) {
-        }
-      });
-    });
-  });
+async function test() {
+  const publisherObj = new Publisher();
+  const campaignServiceObj = new CampaignService();
+  const engine = new Engine(publisherObj, campaignServiceObj);
+
+  const active = await engine.fethActiveCampaigns(58922);
+  const matched = await engine.matchedCampaigns(event1, active);
+  const processWorkflow = await engine.processMatchedCampaigns(event1, matched);
+  console.log(processWorkflow);
 }
 
-matchCampaigns(event1, campaigns);
-processMatchedCampaigns(event1, matched_campagins);
+test();
