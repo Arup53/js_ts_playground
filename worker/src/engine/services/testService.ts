ea22e21ba22e7a7afff2b61034d99fd41fc501e1
Arup53 <arupkumarhowlader12@gmail.com>
2026-02-16 13:39:12 +0600

    fix:fix error in testService

--- worker/src/engine/services/testService.ts
+++ worker/src/engine/services/testService.ts
@@ -1,3 +1,4 @@
+import { CamapaignTypes, Channel } from "../types/types";
 import CampaignService from "./campaignService";
 
 async function test() {
@@ -5,9 +6,33 @@ async function test() {
 
   await campaignService.init();
   console.log("connected sucessfully to redis");
+
+  // const campaignObj = {
+  //   tenant_id: 58922,
+  //   campaign_id: 1,
+  //   campaign_name: "sign_up",
+  //   campaign_type: CamapaignTypes.event,
+  //   trigger: {
+  //     event_name: "sign_up",
+  //     conditions: [{ base_condition: true }],
+  //   },
+  //   actions: {
+  //     action: [{ channel: Channel.sms, message: "Hello there" }],
+  //   },
+  //   duration: "26th feb",
+  //   frequency: 1,
+  //   entries_customers: {},
+  //   active: true,
+  //   created_at: "1january",
+  // };
+
+  // console.log(campaignObj);
+
+  // await campaignService.createCampaign(campaignObj);
+
   const result = await campaignService.getCampaignsOfTenantByStatus(
     58922,
-    "active"
+    true
   );
   console.log(result);
 
