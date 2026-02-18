1f5593796ad3c35b75db6c98ed7df2057551d902
Arup53 <arupkumarhowlader12@gmail.com>
2026-02-18 19:01:27 +0600

    feat(sqsManager): implement sendMessageToSQS method

---
+++ worker/src/sqsManager.ts
@@ -0,0 +1,39 @@
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+
+class SQSManager {
+  private sqs_url;
+  private client;
+  constructor() {
+    this.client = new SQSClient({});
+    this.sqs_url = process.env.SQS_URL;
+  }
+
+  async sendMessageToSQS(message) {
+    if (!this.sqs_url) {
+      throw new Error("SQS_URL is not defined in environment variables");
+    }
+
+    const command = new SendMessageCommand({
+      QueueUrl: this.sqs_url,
+      MessageBody: JSON.stringify(message),
+      MessageAttributes: {
+        tenant_id: {
+          DataType: "Number",
+          StringValue: message.tenant_id.toString(),
+        },
+        event: {
+          DataType: "String",
+          StringValue: message.event,
+        },
+      },
+    });
+
+    try {
+      const response = await this.client.send(command);
+      return response;
+    } catch (error) {
+      console.error("Failed to send SQS message:", error);
+      throw error;
+    }
+  }
+}
