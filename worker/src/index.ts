227203785ff5498690c9f83716574651d26cbd86
Arup53 <arupkumarhowlader12@gmail.com>
2026-02-18 19:37:20 +0600

    featur(sqsManager): create sqsManagerInstance

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -2,18 +2,22 @@ import { createClient } from "redis";
 import Publisher from "./engine/publisher";
 import CampaignService from "./engine/services/campaignService";
 import Engine from "./engine/engine";
+import SQSManager from "./sqsManager";
 
 async function main() {
-  const redisClient = createClient({
-    url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
-  });
+  // const redisClient = createClient({
+  //   url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
+  // });
 
-  redisClient.on("error", (err) => {
-    console.error("Redis error:", err);
-  });
+  // redisClient.on("error", (err) => {
+  //   console.error("Redis error:", err);
+  // });
 
-  await redisClient.connect();
-  console.log("Worker connected to Redis");
+  // await redisClient.connect();
+  // console.log("Worker connected to Redis");
+
+  const sqsManager = new SQSManager();
+  console.log("Worker connected to SQS");
 
   const publisher = new Publisher();
   const campaignService = new CampaignService();
@@ -21,20 +25,25 @@ async function main() {
 
   while (true) {
     try {
-      const result = await redisClient.brPop("event_queue", 0);
+      // const result = await redisClient.brPop("event_queue", 0);
 
-      if (!result) continue;
+      // if (!result) continue;
 
-      const event = JSON.parse(result.element);
+      // const event = JSON.parse(result.element);
 
+      // console.log("Processing event:", event);
+
+      // await engine.process(event);
+
+      // console.log(
+      //   "Length of queue after processing",
+      //   await redisClient.lLen("event_queue")
+      // );
+
+      const event = await sqsManager.dequeue();
+      if (!event) continue;
       console.log("Processing event:", event);
-
       await engine.process(event);
-
-      console.log(
-        "Length of queue after processing",
-        await redisClient.lLen("event_queue")
-      );
     } catch (err) {
       console.error("Worker error:", err);
     }
