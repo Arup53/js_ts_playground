2d27b4e909d4797ce48c2a3e0ec27dceaea7ac6b
Arup53 <arupkumarhowlader12@gmail.com>
2026-02-18 10:45:36 +0600

    feat(publisher): implement constructor

--- worker/src/engine/publisher.ts
+++ worker/src/engine/publisher.ts
@@ -1,4 +1,18 @@
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
+
 export class Publisher {
+  private client;
+  private arn_topic_container;
+
+  constructor() {
+    this.client = new SNSClient({ region: "us-east-1" });
+    this.arn_topic_container = {
+      email: process.env.EMAIL_TOPIC,
+      sms: process.env.SMS_TOPIC,
+      slack: process.env.SLACK_TOPIC,
+    };
+  }
+
   async publish(channel, message) {
     const parsed_message = JSON.parse(message);
     console.log("Channel is ", channel);
