394f1237704dcedc9a9057f9292c8fc28f1e56b5
Arup53 <arupkumarhowlader12@gmail.com>
2026-02-19 08:57:51 +0600

    feat(index): add import statement for secrets

--- api/index.ts
+++ api/index.ts
@@ -3,20 +3,20 @@ import { anonEventsRegisters } from "./routes/events_register_anon";
 import { postAnonymousEvents } from "./routes/postAnonymousEvents";
 
 const app = express();
-const server_port = 3000;
+const server_port = process.env.port || 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 // app.get("/", (req, res) => {
 //   console.log("Received request to ", req);
 //   res.json({ message: "Hello from server" });
 // });
-app.get('/',async(req,res)=>{
-  res.send("Hello from server")
-})
-app.use("/api/routes/events_register_anon",anonEventsRegisters)
+app.get("/", async (req, res) => {
+  res.send("Hello from server");
+});
+app.use("/api/routes/events_register_anon", anonEventsRegisters);
 
-app.use("/api/routes/events/anonymous",postAnonymousEvents)
+app.use("/api/routes/events/anonymous", postAnonymousEvents);
 
 try {
   const server = app.listen(server_port, () => {
