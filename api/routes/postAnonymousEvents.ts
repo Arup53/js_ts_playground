36f691a1ed1a007f5fe817cafeb28d5498ca5d31
Arup53 <arupkumarhowlader12@gmail.com>
2026-02-19 08:52:18 +0600

    feat(postAnonymousEvents): add types

--- api/routes/postAnonymousEvents.ts
+++ api/routes/postAnonymousEvents.ts
@@ -1,24 +1,31 @@
-import { Router } from "express";
+import { Router, type Request, type Response } from "express";
+
 import { RedisManager } from "../RedisManager";
+import SQSManager from "../aws_clients/sqsManager";
+import type { PostEventsBody } from "../types/types";
 
 export const postAnonymousEvents = Router();
 
-postAnonymousEvents.post("/", async (req, res) => {
-  const { events: eventsBatch } = req.body;
+postAnonymousEvents.post(
+  "/",
+  async (req: Request<{}, {}, PostEventsBody>, res: Response) => {
+    const { events: eventsBatch } = req.body;
 
-  const redis = await RedisManager.getInstance();
+    // const redis = await RedisManager.getInstance();
+    const sqsManager = new SQSManager();
 
-  for (const event of eventsBatch) {
-    try {
-      const response = await redis.addEventInQueue(event);
-      console.log("Succesfull event pushing in Queue", response);
-    } catch (e) {
-      throw new Error(
-        "Operation Failed to send event to sqs for event:",
-        event.event
-      );
+    for (const event of eventsBatch) {
+      try {
+        // const response = await redis.addEventInQueue(event);
+        const response = await sqsManager.enqueue(event);
+        console.log("Succesfull event pushing in Queue", response);
+      } catch (e) {
+        throw new Error(
+          "Operation Failed to send event to sqs for event:" + event.event
+        );
+      }
     }
-  }
 
-  res.json({ message: "Events added to queue succesfully" });
-});
+    res.json({ message: "Events added to queue succesfully" });
+  }
+);
