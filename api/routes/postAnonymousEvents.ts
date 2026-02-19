8fca5389ab38635218eee815eda2f19afba52677
Arup53 <arupkumarhowlader12@gmail.com>
2026-02-19 08:33:34 +0600

    feat(postAnonymousEvents): add try catch block in for loop

--- api/routes/postAnonymousEvents.ts
+++ api/routes/postAnonymousEvents.ts
@@ -1,20 +1,24 @@
-import {Router} from "express";
+import { Router } from "express";
 import { RedisManager } from "../RedisManager";
 
 export const postAnonymousEvents = Router();
 
-postAnonymousEvents.post('/', async (req , res)=>{
+postAnonymousEvents.post("/", async (req, res) => {
+  const { events: eventsBatch } = req.body;
 
-    const {events: eventsBatch}= req.body;
+  const redis = await RedisManager.getInstance();
 
-    const redis = await RedisManager.getInstance();
-
-
-    for (const event of eventsBatch){
+  for (const event of eventsBatch) {
+    try {
       const response = await redis.addEventInQueue(event);
       console.log("Succesfull event pushing in Queue", response);
+    } catch (e) {
+      throw new Error(
+        "Operation Failed to send event to sqs for event:",
+        event.event
+      );
     }
+  }
 
-    res.json({message:"Events added to queue succesfully"})
-
-} )
+  res.json({ message: "Events added to queue succesfully" });
+});
