4d2af6966845ee4cf1728c2b7c4c30ce94122ef2
Arup53 <arupkumarhowlader12@gmail.com>
2026-02-21 11:19:27 +0600

    feat(sqsSubscriberOfSNSTopic): implement parseSNSEnvelope method

---
+++ worker/src/engine/sqsSubscriberOfSNSTopic.ts
@@ -0,0 +1,37 @@
+import { SQSClient } from "@aws-sdk/client-sqs";
+
+class sqsConsumerOfSNSTopic {
+  private reigon: string | null;
+  private sqs_url_for_sns: string | null;
+  private sns_url: string | null;
+  private client;
+
+  constructor() {
+    if (
+      !process.env.REGION ||
+      !process.env.SQS_URL_FOR_SNS ||
+      !process.env.SNS_URL
+    ) {
+      throw new Error(
+        "Region or SQS URL or SNS url not correct in sqsConsumer"
+      );
+    }
+
+    this.reigon = process.env.REGION;
+    this.sqs_url_for_sns = process.env.SQS_URL_FOR_SNS;
+    this.sns_url = process.env.SNS_URL;
+    this.client = new SQSClient(this.reigon);
+  }
+
+  parseSNSEnvelope(sqsMessage) {
+    try {
+      const snsEnvelope = JSON.parse(sqsMessage.Body!);
+      // If RawMessageDelivery is false (default), SNS wraps in envelope
+      const innerMessage = JSON.parse(snsEnvelope.Message);
+      return innerMessage;
+    } catch (err) {
+      console.error("Failed to parse message:", err);
+      return null;
+    }
+  }
+}
