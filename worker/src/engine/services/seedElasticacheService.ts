a95fc98662b2189974e8245a926edf37d4288666
Arup53 <arupkumarhowlader12@gmail.com>
2026-02-24 11:26:44 +0600

    feat(seedElasticacheService):implement seed redis instance in elasticache

---
+++ worker/src/engine/services/seedElasticacheService.ts
@@ -0,0 +1,50 @@
+import { CamapaignTypes, Channel } from "../types/types";
+import CampaignService from "./campaignService";
+
+async function main() {
+  const campaignService = new CampaignService();
+
+  await campaignService.init();
+  console.log("connected sucessfully to redis");
+
+  const campaignObj1 = {
+    tenant_id: 58922,
+    campaign_id: 1,
+    campaign_name: "sign_up",
+    campaign_type: CamapaignTypes.event,
+    trigger: {
+      event_name: "sign_up",
+      conditions: [{ base_condition: true }],
+    },
+    actions: {
+      action: [{ channel: Channel.email, message: "Hello there" }],
+    },
+    duration: "26th feb",
+    frequency: 1,
+    entries_customers: {},
+    active: true,
+    created_at: "1january",
+  };
+  const campaignObj2 = {
+    tenant_id: 58922,
+    campaign_id: 1,
+    campaign_name: "sign_up",
+    campaign_type: CamapaignTypes.event,
+    trigger: {
+      event_name: "page_view",
+      conditions: [{ base_condition: true }],
+    },
+    actions: {
+      action: [{ channel: Channel.sms, message: "Hello there" }],
+    },
+    duration: "1th feb",
+    frequency: 1,
+    entries_customers: {},
+    active: true,
+    created_at: "30th march",
+  };
+  await campaignService.createCampaign(campaignObj1);
+  await campaignService.createCampaign(campaignObj2);
+}
+
+main();
