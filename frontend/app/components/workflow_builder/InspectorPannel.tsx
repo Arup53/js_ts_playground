7fc49833615a10ef9bd60cd1102709969101299e
Arup53 <arupkumarhowlader12@gmail.com>
2026-03-11 12:04:09 +0600

    feat(components/workflowbuilder):implement inspectorpannel

---
+++ frontend/app/components/workflow_builder/InspectorPannel.tsx
@@ -0,0 +1,16 @@
+import { useWorkflowStore } from "@/app/store/workflow/useWorkflow";
+import { useReactFlow } from "@xyflow/react";
+
+function InspectorPanel() {
+  const activeNode = useWorkflowStore((s) => s.activeNode);
+  const nodes = useReactFlow().getNodes();
+
+  const node = nodes.find((n) => n.id === activeNode);
+
+  return (
+    <div>
+      <h3>Inspector</h3>
+      <p>{node?.data?.label}</p>
+    </div>
+  );
+}
