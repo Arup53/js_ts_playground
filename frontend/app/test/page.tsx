7bd432aca43a403710920ffa341091548712a3d5
Arup53 <arupkumarhowlader12@gmail.com>
2026-03-11 12:10:25 +0600

    chore(app/test):import InspectorPannel

--- frontend/app/test/page.tsx
+++ frontend/app/test/page.tsx
@@ -18,6 +18,7 @@ import { Sidebar } from "../components/workflow_builder/sidebar/Sidebar";
 import { DnDProvider } from "../hooks/workflow/dnd/useDnD";
 import { MultiHandleNode } from "../components/nodes/customNode";
 import { useWorkflowStore } from "../store/workflow/useWorkflow";
+import InspectorPanel from "../components/workflow_builder/InspectorPannel";
 
 const nodeTypes = {
   multiHandle: MultiHandleNode,
